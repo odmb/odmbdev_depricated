eefbdf07973d7ad5011e3b6caf9c60dde70fc867
manuelf <>
2013-05-15 03:45:06 +0000

    Made a bit more general the BUILD_HOME folder

--- compile.sh
+++ compile.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-export BUILD_HOME=/home/odmb/mfs/TriDAS
+current_path=`pwd -P`
+if [[ $current_path == *TriDAS* ]]; then
+    export BUILD_HOME=`echo $current_path | sed -r 's|(TriDAS).*$|\1|'`
+else
+    export BUILD_HOME=`dirname ${current_path}`/TriDAS
+fi
 export XDAQ_ROOT=/opt/xdaq
 make
 make install
